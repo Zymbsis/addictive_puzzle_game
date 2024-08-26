7fa9b80015c6a4d6fd9b8d45b283791cfebfd387
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2024-08-26 13:34:18 +0200

    fix-location

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -15,7 +15,7 @@ export function onNavigationLinkClick(e) {
     location.href.includes('term_of_use')
   ) {
     localStorage.setItem('elementId', elementId);
-    location.href = '/index.html';
+    location.href = './index.html';
     return;
   }
   e.preventDefault();
